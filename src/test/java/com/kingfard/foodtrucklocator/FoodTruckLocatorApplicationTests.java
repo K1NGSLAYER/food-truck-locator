8c2d9c0a95021fed75bb0f19c92d596910c68e35
Kingslayer <kingfardthe1st@gmail.com>
2020-06-14 15:47:00 -0500

    Base code

---
+++ src/test/java/com/kingfard/foodtrucklocator/FoodTruckLocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingfard.foodtrucklocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodTruckLocatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
